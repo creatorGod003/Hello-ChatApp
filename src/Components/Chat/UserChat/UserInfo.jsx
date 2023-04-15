4de66178b765449675825e201be6b9a8d07aa03b
Ashutosh Ranjan <ranjan.ashutosh2003@gmail.com>
2023-04-15 23:31:37 +0530

    minor bug issue resolve

--- src/Components/Chat/UserChat/UserInfo.jsx
+++ src/Components/Chat/UserChat/UserInfo.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 // User info component which is responsible for showing user info at the top of chat panel
 const UserInfo = (props) => {
 
   const navigate = useNavigate();
+  const isMobile = useSelector((state)=>{return state.responsive.isMobile}) 
 
   return (
     <div className="bg-blue-400 flex items-center ">
       
-        <button onClick={()=>{navigate(-1)}}>
+        {
+          isMobile?<button onClick={()=>{navigate(-1)}}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -24,7 +27,8 @@ const UserInfo = (props) => {
               d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
             />
           </svg>
-        </button>
+        </button>:null
+        }
         <div className="flex items-center mx-4">
           <div className="flex items-center">
             <img
--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div className='flex flex-col justify-around items-center p-4'>
+    <div className='flex flex-col justify-around items-center p-4 min-h-screen bg-gradient-to-b to-teal-300 from-indigo-500'>
         <div className='border-blue-600 border-4 inline-block rounded-full p-4'>
-          <img src="Images/hello.gif" alt="" className='w-40 h-40' />
+          <img src="Images/hello.gif" alt="" className='w-40 h-40 rounded-full' />
         </div>
         <div className='text-center'>
           <header className='font-bold text-3xl'>About Us</header>
-          <main className='my-20 first-letter:text-5xl'><span className="font-mono text-3xl">Hello</span> a text based chatting app. Implementing latest technologies. Connect you with our world. <br /><span className='my-8 block'>Have fun!</span></main>
+          <main className='my-20 first-letter:text-5xl text-2xl'><span className="font-mono text-3xl">Hello</span> a text based chatting app. Implementing latest technologies. Connect you with our world. <br /><span className='my-8 block'>Have fun!</span></main>
           <footer>
             <Link to="/home" className='bg-blue-400 p-4 text-white rounded-lg hover:bg-white hover:text-black hover:border-blue-700 hover:border-4 cursor-pointer '>
               Contact Us
